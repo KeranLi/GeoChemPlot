697073214d556e0a98aa63c9927f7c36a2892ba8
‘KeranLi’ <keranli98@outlook.com>
2023-12-10 14:59:45 +0800

    Change to layout and add a canvas.

--- GUI.py
+++ GUI.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QWidget, QPushButton, QLabel, QComboBox, QVBoxLayout, QLineEdit, QProgressBar
 from PyQt5.QtCore import Qt
 from dataloader import FileSelector
 from Analysis_tool.rare_earth_element import Normalization
@@ -13,52 +13,73 @@ class Main_GUI(QWidget):
         self.path = ''
         self.normalization_method = ''
         self.txts = []
+        self.file_selector = FileSelector()  # 创建FileSelector对象
+        self.path_label = QLabel()
         self.createWidget()
 
     def createWidget(self):
-        self.btn11 = QPushButton('选择文件', self)
-        self.btn11.clicked.connect(self.select_file)
-        self.btn11.setGeometry(10, 10, 100, 30)
-        self.btn12 = QPushButton('帮助', self)
-        self.btn12.setGeometry(690, 10, 100, 30)
+        # 创建布局
+        layout = QVBoxLayout()
 
-        self.label31 = QLabel('选择的文件路径:', self)
-        self.label31.setGeometry(10, 60, 100, 30)
-        self.label32 = QLabel(self.path, self)
-        self.label32.setGeometry(120, 60, 500, 30)
+        # 选择文件的按钮
+        self.select_file_button = QPushButton('选择文件', self)
+        self.select_file_button.setGeometry(10, 10, 100, 30)
+        self.select_file_button.clicked.connect(self.select_file)
+        layout.addWidget(self.select_file_button)
+        
+        self.help_button = QPushButton('帮助', self)
+        self.help_button.setGeometry(690, 10, 100, 30)
+        layout.addWidget(self.help_button)
 
-        self.label41 = QLabel('归一化方法:', self)
-        self.label41.setGeometry(10, 110, 100, 30)
+        self.file_path_show = QLabel('文件路径:', self)
+        self.file_path_show.setGeometry(10, 60, 100, 30)
+        layout.addWidget(self.file_path_show)
+
+        self.normalized_methods_select_button = QLabel('归一化方法:', self)
+        self.normalized_methods_select_button.setGeometry(10, 110, 100, 30)
+        layout.addWidget(self.normalized_methods_select_button)
+
         self.normalization_combobox = QComboBox(self)
-        self.normalization_combobox.addItems(['Method 1', 'Method 2', 'Method 3'])
+        self.normalization_combobox.addItems(['PAAS', '球粒陨石', 'Sea water'])
         self.normalization_combobox.setGeometry(120, 110, 200, 30)
+        layout.addWidget(self.normalization_combobox)
 
-        self.label42 = QLabel('样本名称:', self)
-        self.label42.setGeometry(10, 160, 100, 30)
-        self.wenjianming_name = QLineEdit(self)
-        self.wenjianming_name.setGeometry(120, 160, 200, 30)
+        self.start_analysis_button = QPushButton('开始分析', self)
+        self.start_analysis_button.clicked.connect(self.data_analysis)
+        self.start_analysis_button.setGeometry(350, 610, 100, 30)
+        layout.addWidget(self.start_analysis_button)
 
-        self.pb = QProgressBar(self)
-        self.pb.setGeometry(120, 210, 200, 30)
-        self.pb.setRange(0, 100)
-        self.pb.setValue(0)
-        self.pb.setOrientation(Qt.Horizontal)
-        self.pb.setFormat('%p%')
+        # self.label42 = QLabel('样本名称:', self)
+        # self.label42.setGeometry(10, 160, 100, 30)
+        # self.wenjianming_name = QLineEdit(self)
+        # self.wenjianming_name.setGeometry(120, 160, 200, 30)
 
-        self.label91 = QLabel('Author: Keran Li', self)
-        self.label91.setGeometry(300, 260, 200, 30)
-        self.label91.setStyleSheet('font-size: 15pt; font-weight: bold; border: 2px solid black;')
+        # self.pb = QProgressBar(self)
+        # self.pb.setGeometry(120, 210, 200, 30)
+        # self.pb.setRange(0, 100)
+        # self.pb.setValue(0)
+        # self.pb.setOrientation(Qt.Horizontal)
+        # self.pb.setFormat('%p%')
+
+        self.author_show = QLabel('Author: Keran Li', self)
+        self.author_show.setGeometry(self.width() - 210, self.height() - 40, 200, 30)
+        self.author_show.setStyleSheet('font-size: 10pt; font-weight: bold; border: 2px solid black;')
+        layout.addWidget(self.author_show)
 
         self.figure = plt.figure(figsize=(6, 4), dpi=100)
         self.canvas = FigureCanvas(self.figure)
         self.canvas.setGeometry(10, 310, 780, 280)
+        layout.addWidget(self.canvas)
 
-        self.btn51 = QPushButton('开始分析', self)
-        self.btn51.clicked.connect(self.data_analysis)
-        self.btn51.setGeometry(350, 610, 100, 30)
+        # 设置布局到主窗口
+        self.setLayout(layout)
 
     def select_file(self):
-        self.selectTxts = FileSelector.selectTxts(self)
+        # 调用FileSelector对象的selectTxts方法选择文件
+        self.file_selector.selectTxts()
+        self.txts = self.file_selector.txts
+        self.path = self.file_selector.path_name
+        self.path_label.setText(self.path)
 
     def data_analysis(self):
         self.data_analysis = Normalization.data_analysis(self)
--- dataloader.py
+++ dataloader.py
@@ -1,21 +1,21 @@
-from tkinter import *
-from tkinter.filedialog import askopenfilenames
-import os 
+from PyQt5.QtWidgets import QFileDialog, QLineEdit
+import os
 
-# 选择文件
 class FileSelector:
    def __init__(self):
        self.txts_name = None
        self.txts = []
        self.path_name = None
-       self.path = None
+       self.path = QLineEdit()
 
    def selectTxts(self):
-       self.txts_name = askopenfilenames()
+       options = QFileDialog.Options()
+       options |= QFileDialog.DontUseNativeDialog
+       self.txts_name, _ = QFileDialog.getOpenFileNames(None, "选择文件", "", "Text Files (*.txt);;All Files (*)", options=options)
        self.txts = []
        for i in self.txts_name:
            self.path_name = os.path.split(i)[0]
            self.txts.append(os.path.split(i)[1]) 
        os.chdir(self.path_name)
        self.path_name = self.path_name
-       self.path.set(self.path_name)
+       self.path.setText(self.path_name)
